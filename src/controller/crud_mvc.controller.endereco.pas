72c4846818bf22ad331600d62da56833ddcc3241
Julio Cesar Andrade <juliocandrade@gmail.com>
2023-05-15 03:18:07 -0300

    Criação do controller para endereco

---
+++ src/controller/crud_mvc.controller.endereco.pas
@@ -0,0 +1,55 @@
+unit crud_mvc.controller.endereco;
+
+interface
+
+uses
+  crud_mvc.controller.interfaces,
+  crud_mvc.model.entidades.endereco,
+  crud_mvc.dao.interfaces;
+
+type
+  TControllerEndereco = class(TInterfacedObject, iControllerEndereco)
+  private
+    FEndereco : TModelEndereco;
+    FDAOEndereco : iDAOEndereco;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    class function New : iControllerEndereco;
+    function Endereco : TModelEndereco;
+    function DAO : iDAOEndereco;
+  end;
+implementation
+
+uses
+  crud_mvc.dao.endereco;
+
+{ TControllerEndereco }
+
+constructor TControllerEndereco.Create;
+begin
+  FEndereco := TModelEndereco.Create;
+  FDAOEndereco := TDAOEndereco.New(FEndereco);
+end;
+
+function TControllerEndereco.DAO: iDAOEndereco;
+begin
+  Result := FDAOEndereco;
+end;
+
+destructor TControllerEndereco.Destroy;
+begin
+  FEndereco.Free;
+  inherited;
+end;
+
+class function TControllerEndereco.New: iControllerEndereco;
+begin
+  Result := Self.Create;
+end;
+
+function TControllerEndereco.Endereco: TModelEndereco;
+begin
+  Result := FEndereco;
+end;
+end.
--- src/controller/crud_mvc.controller.interfaces.pas
+++ src/controller/crud_mvc.controller.interfaces.pas
@@ -4,12 +4,19 @@ interface
 
 uses
   crud_mvc.model.entidades.pessoa,
-  crud_mvc.dao.interfaces;
+  crud_mvc.dao.interfaces,
+  crud_mvc.model.entidades.endereco;
 type
   iControllerPessoa = interface
     ['{EC503F57-4967-4251-866F-B33469E1F809}']
     function Pessoa : TModelPessoa;
     function DAO : iDAOPessoa;
+  end;
+
+  iControllerEndereco = interface
+    ['{A3DF219C-257E-4217-B537-48AC8D222C3D}']
+    function endereco : TModelEndereco;
+    function DAO : iDAOEndereco;
   end;
 implementation
 
